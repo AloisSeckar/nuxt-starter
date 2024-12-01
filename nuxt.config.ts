8c45a4fb424cedf77d80d08078ba93a02a029aff
Alois Sečkár <ellrohir@seznam.cz>
2024-12-01 14:26:22 +0100

    build: update compatibility date

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // Nuxt project configuration like modules or runtime variables goes here
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-09-01',
+  compatibilityDate: '2024-12-01',
   // simple ESLint config
   modules: [
     '@nuxt/eslint',
