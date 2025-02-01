9a5d6f2926143ae17d1dffde1d018e077d96b06a
Alois Sečkár <ellrohir@seznam.cz>
2025-02-01 09:44:30 +0100

    build: update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,13 @@
 // Nuxt project configuration like modules or runtime variables goes here
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-12-01',
   // simple ESLint config
   modules: [
     '@nuxt/eslint',
   ],
+
+  compatibilityDate: '2025-02-01',
+
   eslint: {
     config: {
       stylistic: true,
